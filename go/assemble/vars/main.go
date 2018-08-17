fe8e3ee6f7268e87ad2e874269539f73c9237e3b
Kailiang Ji <jkl@irif.fr>
2018-08-18 00:04:04 +0200

    go: assemble language practice

---
+++ go/assemble/vars/main.go
@@ -0,0 +1,9 @@
+package main
+
+import pkg "./pkg"
+
+func main() {
+	println(pkg.Id)
+	println(pkg.Name)
+	println(len(pkg.Name))
+}
---
+++ go/assemble/vars/pkg/pkg_amd64.s
@@ -0,0 +1,16 @@
+GLOBL ·Id(SB),$8	
+	
+DATA ·Id+0(SB)/1,$0x37
+DATA ·Id+1(SB)/1,$0x25
+DATA ·Id+2(SB)/1,$0x00
+DATA ·Id+3(SB)/1,$0x00
+DATA ·Id+4(SB)/1,$0x00
+DATA ·Id+5(SB)/1,$0x00
+DATA ·Id+6(SB)/1,$0x00
+DATA ·Id+7(SB)/1,$0x00
+
+GLOBL ·Name(SB),$24
+	
+DATA ·Name+0(SB)/8,$·Name+16(SB)
+DATA ·Name+8(SB)/8,$6
+DATA ·Name+16(SB)/8,$"gopher"
